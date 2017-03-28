9f3cff9b1d47708dc0efb5fa33a4ddae3f09a654
Sarah Alexander <sarah@onlyverysmall.com>
2017-03-28 10:18:37 -0700

    add seeds, star wars edition

--- seeds.rb
+++ seeds.rb
@@ -33,3 +33,45 @@ db.execute <<-SQL
     license_key VARCHAR NOT NULL
   );
 SQL
+
+# Seed users
+user_emails = [
+  "rey@test.com", "maz@test.com", "jyn@test.com",
+  "leia_shop@test.com", "bb8_shop@test.com"
+]
+user_emails.each do |email|
+  db.execute("INSERT INTO users (email_address) VALUES (?);", email)
+end
+
+# Grab user ids
+rey, maz, jyn, leia, bb8 = db.execute("SELECT * FROM users;")
+                             .map { |user| user["id"] }
+
+# Seed orders with user ids
+order_pairs = [[leia, rey], [leia, maz], [leia, jyn], [bb8, rey], [bb8, jyn]]
+order_pairs.each do |shop, customer|
+  db.execute(
+    "INSERT INTO orders (user_id_shop, user_id_customer) VALUES (?, ?);",
+    shop, customer
+  )
+end
+
+# Seed license key
+key_text = "Quigoncadeskywalkermustafardarthlarssidiousantillesk3poWedgelandowedgemonantillesnaboosolodantooinejawaCadec3poantillesfettlandoackbarDookuleiawookieehuttHuttchewbaccamustafarendorchewbaccajadecalrissianHuttdarthwookieeantillesowenGrievoushuttskywalkermoffcalamarimoff"
+db.execute(
+  "INSERT INTO license_keys (user_id, license_key) VALUES (?, ?);",
+  rey, key_text
+)
+db.execute(
+  "UPDATE users SET num_license_keys_sent = num_license_keys_sent + 1 WHERE id = ?;",
+  rey
+)
+
+# Print results
+puts "USERS"
+db.execute("SELECT * FROM users;").each { |user| p user }
+puts "ORDERS"
+db.execute("SELECT * FROM orders;").each { |order| p order }
+puts "LICENSE_KEYS"
+db.execute("SELECT * FROM license_keys;").each { |license| p license }
+
