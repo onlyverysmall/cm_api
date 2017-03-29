7f0e6c770948292b5c048927dded6c9363684f79
Sarah Alexander <sarah@onlyverysmall.com>
2017-03-28 17:46:57 -0700

    add specs for route resolver and params checker

---
+++ spec/params_checker_spec.rb
@@ -0,0 +1,14 @@
+require_all Dir.glob("**/**/*.rb").reject { |f| f.start_with? "spec/" }
+require "rack/test"
+
+describe ParamsChecker do
+  include Rack::Test::Methods
+
+  context "POST sans params" do
+    let(:app)      { ParamsChecker.new({}) }
+    let(:response) { post "/license-key" }
+
+    it { expect(response.status).to eq 400 }
+    it { expect(response.body).to include "bad request" }
+  end
+end
---
+++ spec/route_resolver_spec.rb
@@ -0,0 +1,22 @@
+require_all Dir.glob("**/**/*.rb").reject { |f| f.start_with? "spec/" }
+require "rack/test"
+
+describe RouteResolver do
+  include Rack::Test::Methods
+
+  context "GET to '/'" do
+    let(:app)      { RouteResolver.new({}) }
+    let(:response) { get "/" }
+
+    it { expect(response.status).to eq 405 }
+    it { expect(response.body).to include "invalid request method" }
+  end
+
+  context "POST with weird path" do
+    let(:app)      { RouteResolver.new({}) }
+    let(:response) { post "/somewhere-weird" }
+
+    it { expect(response.status).to eq 404 }
+    it { expect(response.body).to include "not found" }
+  end
+end
